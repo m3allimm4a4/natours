708b680884cb4042c4a88e8142ca4e1cb32613ea
Bechara <becharaarahceb2017@gmail.com>
2022-05-08 13:14:26 +0300

    add missing auth to routes

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -12,10 +12,13 @@ const { protect, restrictTo } = require('../middlewares/authMiddlewares');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(protect);
+
+router.route('/').get(getAllReviews).post(restrictTo('user'), setTourUserIds, createReview);
 router
-  .route('/')
-  .get(getAllReviews)
-  .post(protect, restrictTo('user'), setTourUserIds, createReview);
-router.route('/:id').get(getReview).patch(updateReview).delete(deleteReview);
+  .route('/:id')
+  .get(getReview)
+  .patch(restrictTo('user', 'admin'), updateReview)
+  .delete(restrictTo('user', 'admin'), deleteReview);
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -15,23 +15,28 @@ const {
   resetPassword,
   updatePassword,
 } = require('../controllers/authController');
-const { protect } = require('../middlewares/authMiddlewares');
+const { protect, restrictTo } = require('../middlewares/authMiddlewares');
 const { getMe } = require('../middlewares/userMiddlewares');
 
 const router = express.Router();
 
-router.get('/me', protect, getMe, getUser);
-
 router.post('/signup', signup);
 router.post('/signin', signin);
-
 router.post('/forgot-password', forgotPassword);
 router.patch('/reset-password/:token', resetPassword);
 
-router.patch('/update-password', protect, updatePassword);
+// Protect all routes below
+router.use(protect);
 
-router.patch('/update-me', protect, updateMe);
-router.delete('/delete-me', protect, deleteMe);
+router.patch('/update-password', updatePassword);
+
+router.get('/me', getMe, getUser);
+
+router.patch('/update-me', updateMe);
+router.delete('/delete-me', deleteMe);
+
+// Authorize only admins
+router.use(restrictTo('admin'));
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
